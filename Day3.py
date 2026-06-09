5596d75da5c07c702d9fec2ed9162f9d5a54c6f9
kumar-t-chavan <kumartchavan1552@gmail.com>
2026-06-09 12:41:19 +0530

    modified file add new code

---
+++ Day3.py
@@ -0,0 +1,20 @@
+# a  = int(input("Enter a Number : "))
+
+# if a % 2 == 0:
+#     print("Even number")
+
+# else:
+#     print("Odd number")
+
+
+Num = int(input("Enter a Number : "))
+
+if Num>0:
+    print("Number is positive")
+
+elif Num<0:
+    print("Number is Negative")
+
+else:
+    print("Number is Zero")
+     
